2f02fb66d67ee874f40c41ff90477dffb314c24d
blakewatkins <39271690+blakewatkins@users.noreply.github.com>
2019-12-18 09:56:54 -0500

    commit with two topics and Message<byte[]> consumer

--- src/main/java/com/amway/dms/part_three_trial/config/ConsumerConfig.java
+++ src/main/java/com/amway/dms/part_three_trial/config/ConsumerConfig.java
@@ -3,7 +3,9 @@ package com.amway.dms.part_three_trial.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
 
+import java.util.Arrays;
 import java.util.function.Consumer;
 
 @Slf4j
@@ -11,7 +13,8 @@ import java.util.function.Consumer;
 public class ConsumerConfig {
 
   @Bean
-  public Consumer<String> eventConsumer() {
-    return System.out::println;
+  public Consumer<Message<byte[]>> eventConsumer() {
+
+    return input -> System.out.println(Arrays.toString(input.getPayload()));
   }
 }
